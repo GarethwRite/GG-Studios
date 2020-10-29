759efd0c211e7ee212a4681d66c5d5b39e4e5bda
Gareth Wright <garygotcha27@gmail.com>
2020-10-29 14:56:54 +1300

    linked pages and started animated menu bar partial

--- routes.js
+++ routes.js
@@ -2,19 +2,19 @@ const fs = require('fs')
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  const name = req.query.name || 'person'
-  const viewData = {
-    name: name
-  }
-  res.render('home', viewData)
-})
+
 
 
 router.get('/', (req, res) => {
- 
   res.render('home')
   
+})
+router.get('/about', (req,res) => {
+  res.render('about')
+})
+
+router.get('/newsletter', (req,res) => {
+  res.render('newsletter')
 })
 
 
